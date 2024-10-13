e65cdd38bf6ade9476e569688d87a1b2a22e78c0
raselcsedev <raselcse.dev@gmail.com>
2024-10-13 16:11:20 +0600

    marge store array

---
+++ merge-store-array/merge_store_array.js
@@ -0,0 +1,27 @@
+const merge = (nums1, m, nums2, n) => {
+ 
+    let p1 = m - 1;
+    let p2 = n - 1;
+
+    for (let p = m + n - 1; p >= 0; p--) {
+        
+        if (p2 < 0) {
+            break; 
+        }
+        if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+            nums1[p] = nums1[p1];
+            p1--;
+        } else {
+            nums1[p] = nums2[p2];
+            p2--;
+        }
+    }
+}
+
+let nums1 = [1, 2, 3, 0, 0, 0];
+let m = 3;
+let nums2 = [2, 5, 6];
+let n = 3;
+
+merge(nums1, m, nums2, n);
+console.log(nums1);
