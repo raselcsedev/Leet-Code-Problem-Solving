781687a8857c8152455a36e79eac04091d104ef4
raselcsedev <raselcse.dev@gmail.com>
2024-12-06 21:30:58 +0600

    Remove Duplicates from Sorted Array II

---
+++ Remove Duplicates from Sorted Array II/Remove Duplicates from Sorted Array II.js
@@ -0,0 +1,12 @@
+function removeDuplicates(nums) {
+  let i = 0;
+  for (let num of nums) {
+    if (i < 2 || nums[i - 2] !== num) {
+      nums[i] = num;
+      i++;
+    }
+  }
+  return i;
+}
+
+const nums = [1, 1, 1, 1, 2, 2, 2, 3];
